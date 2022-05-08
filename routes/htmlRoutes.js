42672fa5c25b902a0d054c5f3b2afc1ade352837
gordonlc18 <gordonlc18@gmail.com>
2022-05-08 16:44:11 -0400

    Add Heroku

---
+++ routes/htmlRoutes.js
@@ -0,0 +1,17 @@
+const db = require("../db/db.json");
+const path = require('path');
+const express = require('express');
+
+var app = express();
+
+
+module.exports = function(app) {
+
+    app.get("/", function (req, res) {
+        res.sendFile(path.join(__dirname, "index.html"))
+    });
+
+    app.get('/notes', function (req, res) {
+        res.sendFile(path.join(__dirname, "../public/notes.html"))
+    });
+}
